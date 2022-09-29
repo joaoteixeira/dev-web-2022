e75577686d137055172b337388b83443cd1a81a8
joaoteixeira <joao.teixeira@ifro.edu.br>
2022-09-28 22:04:26 -0400

    Adição de controller de chamado

---
+++ aula-28-09-levensoft/controllers/chamadoController.js
@@ -0,0 +1,37 @@
+// Controller de Chamados
+
+function list(request, response) {
+  response.render('chamado/listagem');
+}
+
+function show(request, response) {
+  const { id } = request.params;
+
+  response.send('chamado ID: ' + id);
+}
+
+function create_form(request, response) {
+  response.render('chamado/abrir-form');
+}
+
+function create(request, response) {
+  response.send('create save');
+}
+
+function update_form(request, response) {
+  response.send('update form');
+}
+
+function update(request, response) {
+  response.send('update save');
+}
+
+function _delete_form(request, response) {
+  response.send('delete form');
+}
+
+function _delete(request, response) {
+  response.send('delete save');
+}
+
+module.exports = { list, show, create_form, create, update_form, update, _delete, _delete_form }
