2262cec91eea1d3a7aac13bd9e56dc19f2824606
joaoteixeira <joao.teixeira@ifro.edu.br>
2022-09-28 20:22:08 -0400

    atualização 21-09

--- aula-21-09-levensoft/routes/base.js
+++ aula-21-09-levensoft/routes/base.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/dashboard', function (request, response) {
+    response.render('index');
+});
+
 router.get('/', function (request, response) {
     let user = { 
         nome: 'Joao', 
@@ -20,6 +24,21 @@ router.get('/login', function (request, response) {
 
 router.get('/register', function (request, response) {
     response.render('auth/register');
+});
+
+router.get('/chamados/abrir', function (request, response) {
+    let data = new Date();
+    let mes = (data.getMonth()+1).toString().padStart(2, '0');
+    let dia = data.getDate().toString().padStart(2, '0');
+    data = data.getFullYear() + '-' + mes +'-' + dia;
+
+    response.render('chamado/abrir-form', { 
+        data
+    });
+});
+
+router.get('/chamados', function (request, response) {
+    response.render('chamado/listagem');
 });
 
 
